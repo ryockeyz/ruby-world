def22b95d918e4b81420141b9cdd129ea07a1b92
ryockeyz <tkryoh@gmail.com>
2025-03-26 13:59:46 +0900

    バックスラッシュ記法

--- string.rb
+++ string.rb
@@ -3,3 +3,10 @@ hello = String.new("Hello")
 puts hello
 puts "こんにちは"
 puts 'Bonjour'
+# バックスラッシュ記法
+puts "こんにちは\nお元気ですか"
+puts "こんにちは\"バックスラッシュ\"さん"
+puts "改行は\\nと記述します"
+puts 'こんにちは\nお元気ですか'
+puts 'こんにちは\'バックスラッシュ\'さん'
+puts 'バックスラッシュは\\'
