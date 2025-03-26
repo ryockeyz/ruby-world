2bbb0f609e60c92d8a80f9bfe7944152f77256ac
ryockeyz <tkryoh@gmail.com>
2025-03-26 14:03:03 +0900

    数値オブジェクト

---
+++ numeric.rb
@@ -0,0 +1,20 @@
+# 整数
+puts 10
+puts 10.class
+# 浮動小数
+puts 3.14
+puts 3.14.class
+# マイナス
+puts -10
+puts -3.14
+# 10進数
+puts 214
+# 2進数 先頭に0b
+puts 0b11010110
+# 8進数 先頭に0
+puts 0326
+# 16進数 先頭に0x
+puts 0xD6
+# 指数
+puts 2.5e5
+puts 2.5e-4
