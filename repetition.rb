4871535733f44b25cbbbdf4310176c4d2bdd5de9
ryockeyz <tkryoh@gmail.com>
2025-03-26 15:55:29 +0900

    範囲オブジェクト

--- repetition.rb
+++ repetition.rb
@@ -21,3 +21,27 @@ end
 for num in 1..3
   puts num
 end
+
+# 範囲オブジェクト
+# first..last 最後の値まで含まれる
+# first...last 最後の値は含まれない
+for num in 5..10
+  puts num
+end
+
+for num in "g"..."j"
+  puts num
+end
+# Rangeオブジェクトを使って範囲オブジェクトを作成
+# Range.new(first,last[, exclude_end]) exclude_endはtrueの場合は最後の値を含めない
+for num in Range.new(11, 15, true)
+  puts num
+end
+
+# for文における範囲オブジェクトの扱いについて
+# 渡した値の次の値がどうなるかは渡したオブジェクトに定義されているsuccメソッドによる
+# Integerの場合は次の数字は1加算した値
+# 文字列の場合はaだったらb、azだったらba
+for num in "ax"..."bc"
+  puts num
+end
