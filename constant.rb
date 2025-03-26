5a182fad2e7b08d13ddfe6529341c7ff2a6b7dce
ryockeyz <tkryoh@gmail.com>
2025-03-26 14:47:40 +0900

    組み込み定数

--- constant.rb
+++ constant.rb
@@ -5,3 +5,14 @@
 PI = 3.14
 menseki = 5 * 5 * PI
 puts menseki
+
+# 組み込み定数
+# Rubyであらかじめ定義されている定数
+# RUBY_で始まる定数はRubyに関する情報が格納されている
+puts RUBY_COPYRIGHT
+# STDIN、STDOUT、STDERRはそれぞれ標準入力、標準出力、標準エラー出力を表すもので、それぞれ$stdin、$stdout、$stderrのデフォルト値
+# ENVは環境変数を表す連想配列が格納されている
+p ENV
+# 引数
+# ARGVはRubyスクリプトに与えられた引数を表す配列が格納されている
+p ARGV
