58d4125faf1bca9a96dbbd8d94a6da92103440bf
ryockeyz <tkryoh@gmail.com>
2025-04-08 13:53:53 +0900

    ハッシュオブジェクトから値を取得

--- hash.rb
+++ hash.rb
@@ -1,3 +1,16 @@
 # ハッシュオブジェクトの作成
 hash1 = {"Red" => 1, "Blue" => 2, "Green" => 3}
 puts hash1
+
+# ハッシュオブジェクトから値を取得
+# 存在しないキーを指定するとnilが返却される
+p hash1["Red"]
+p hash1["Blue"]
+p hash1["Yellow"]
+
+# fetchメソッドを使って値を取得
+# ハッシュオブジェクト.fetch(key[, default])
+# keyが存在せずdefaultが設定されていたらその値を返す
+p hash1.fetch("Red")
+p hash1.fetch("Blue")
+p hash1.fetch("Yellow", "Error")
