d7bc6666603884568e704caf3a65ff7ac48ee15d
ryockeyz <tkryoh@gmail.com>
2025-04-12 20:03:26 +0900

    引数ありのメソッド呼び出し

--- method.rb
+++ method.rb
@@ -15,7 +15,24 @@ puts self.class.to_s
 # ルール1：1文字目は英文字かアンダーバー
 # ルール2：2文字目以降は英数文字かアンダーバー
 # 大文字から開始することも可能、あまり大文字からは始めない、大文字はじまりの場合カッコは省略できない
-def printHello str
+def printHello
+  puts "Hello"
+end
+printHello
+
+# 引数ありのメソッド呼び出し
+# メソッドを呼び出す側で指定した引数を「実引数」、メソッド定義側で記述した引数を「仮引数」と呼ぶこともある
+def printStr str
   puts str
 end
-printHello "Hello"
+printStr "Hello"
+
+def printMsgName name, msg = "Hello"
+  puts "#{msg}, #{name}"
+end
+printMsgName "taro"
+printMsgName "taro", "Goodbye"
+
+def addString str
+  str << ", Japan"
+end
