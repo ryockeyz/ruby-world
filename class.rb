130cfc35ff309cdb82d0ba3035a20f989cc84ae6
ryockeyz <tkryoh@gmail.com>
2025-04-18 08:40:19 +0900

    クラスの定数定義

--- class.rb
+++ class.rb
@@ -4,6 +4,11 @@
 # attr_writer :変数名	更新が可能
 # attr_accessor :変数名	参照と更新が可能
 class Car
+  # 定数
+  # 大文字始まりで定義すると定数になる、すべて大文字で書くことが多い
+  # メソッド内には定義できない
+  MAKER = "toyota"
+
   def initialize(car_name = "未定義")
     @name = car_name
   end
@@ -13,3 +18,4 @@ end
 car = Car.new()
 car.name = "crown"
 puts car.name
+puts Car::MAKER
